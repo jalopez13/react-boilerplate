a352085439951b5e9e96a0fa13b54723800ddc75
Javier Lopez <javier.lopez@tmz.com>
2019-11-14 17:36:52 -0800

    Added react react-dom cdn links to prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,20 @@
 const merge = require('webpack-merge');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
-  mode: 'production'
+  mode: 'production',
+ 
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle_sizes.html'
+    })
+  ],
+
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
 });
