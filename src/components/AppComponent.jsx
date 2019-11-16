1056d187a56cd0e2e15608b43c54fbc8efedda58
Javier Lopez <javier.lopez@tmz.com>
2019-11-15 16:34:59 -0800

    Added react-axe for accessibility checking

--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div className="app-component">Start coding...</div>;
+    return (
+      <div className="app-component">
+        <h1>React Boilerplate</h1>
+        <p>React Starter Boilerplate</p>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './style.scss';
 import App from './components/AppComponent';
 
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe');
+
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
